dc8a5385ed35179dd328ecb4c1c517cc4060055e
Vini Mendes <viniciussmendes@gmail.com>
2018-10-26 12:16:29 -0300

    Initial commit.

--- app/src/main/java/br/edu/ifsul/primeiroapp/activity/ProdutosActivity.java
+++ app/src/main/java/br/edu/ifsul/primeiroapp/activity/ProdutosActivity.java
@@ -2,9 +2,16 @@ package br.edu.ifsul.primeiroapp.activity;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -34,7 +41,7 @@ import br.edu.ifsul.primeiroapp.barcode.BarcodeCaptureActivity;
 import br.edu.ifsul.primeiroapp.model.Produto;
 import br.edu.ifsul.primeiroapp.setup.AppSetup;
 
-public class ProdutosActivity extends AppCompatActivity {
+public class ProdutosActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private static final int RC_BARCODE_CAPTURE = 1;
     private ListView lvProdutos;
@@ -78,7 +85,22 @@ public class ProdutosActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_produtos);
+        setContentView(R.layout.activity_main);
+
+        //configuracoes da navegation view
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
 
         lvProdutos = findViewById(R.id.lvProdutos);
         lvProdutos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -211,5 +233,10 @@ public class ProdutosActivity extends AppCompatActivity {
 
     private void atualizarView() {
         lvProdutos.setAdapter(new ProdutosAdapter(ProdutosActivity.this, produtos));
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
     }
 }
