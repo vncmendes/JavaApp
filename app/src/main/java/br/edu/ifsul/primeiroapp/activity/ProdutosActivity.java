76ab281be86ee4fcee5cb26e26af37a833704e8e
Vini Mendes <viniciussmendes@gmail.com>
2018-11-30 11:01:51 -0200

    navegacao de alto nivel e tal 30/11/18

--- app/src/main/java/br/edu/ifsul/primeiroapp/activity/ProdutosActivity.java
+++ app/src/main/java/br/edu/ifsul/primeiroapp/activity/ProdutosActivity.java
@@ -97,7 +97,7 @@ public class ProdutosActivity extends AppCompatActivity implements NavigationVie
             public void onDataChange(DataSnapshot dataSnapshot) {
                 // This method is called once with the initial value and again
                 // whenever data at this location is updated.
-
+                Log.d(TAG, "Value is: \n" + dataSnapshot);
                 if (dataSnapshot.getValue() != null) {
                     Log.d(TAG, "dados do firebase " + dataSnapshot.getValue());
 
